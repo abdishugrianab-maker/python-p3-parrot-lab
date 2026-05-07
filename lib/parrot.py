f2abfaeb2eb92b64f02f2454a7dac6b4eb20ba80
abdishugrianab-maker <abdishugrianab@gmail.com>
2026-05-07 16:21:51 +0300

    done

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,12 @@
-def parrot():
-    pass
+def parrot(phrase="Squawk!"):
+    """
+    Prints and returns the given phrase.
+    
+    Args:
+        phrase (str): The phrase to print and return. Defaults to "Squawk!"
+    
+    Returns:
+        str: The phrase that was passed in
+    """
+    print(phrase)
+    return phrase
